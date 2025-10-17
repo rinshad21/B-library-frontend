8fe310a4357479e564b855834d31b834183a5d6d
rinshad21 <mdrinshadka@gmail.com>
2025-10-17 14:44:32 +0530

    loading component on app loading

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,6 @@ import Loading from "./components/Loading";
 
 function App() {
   const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
-  if (isLoading) {
-    return <Loading />;
-  }
   const location = useLocation();
   //admin routes hiding
   const hideNavbarRoutes = ["/admin", "/dashboard"];
@@ -23,13 +13,24 @@ function App() {
   const shouldHideNavbar = hideNavbarRoutes.some((path) =>
     location.pathname.startsWith(path)
   );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <AuthProvider>
-        <main className="bg-gray-200 min-h-screen mx-auto px-4 py-6">
-          {!shouldHideNavbar && <Navbar />}
-          <Outlet />
-        </main>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <main className="bg-gray-200 min-h-screen mx-auto px-4 py-6">
+            {!shouldHideNavbar && <Navbar />}
+            <Outlet />
+          </main>
+        )}
       </AuthProvider>
     </>
   );
